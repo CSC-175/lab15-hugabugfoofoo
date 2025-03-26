3b6d03341adebbdc8acccb9da64f2547c1584fb1
Noah <hugabug@umich.edu>
2025-03-26 13:01:14 -0400

    First Commit

--- lab15.cpp
+++ lab15.cpp
@@ -1,11 +1,62 @@
 #include<iostream>
+#include <limits>
 using namespace std;
 
 // Function prototypes
-void getInfo(int&, int&);
+void getInfo(int &num1, int &num2);
 double computeWays(int, int);
-double factorial(int);
+double factorial(int number) {
+    if (number == 0) {
+        return 1;
+    }else {
+        return number * factorial(number - 1);
+    }
+}
+void getInfo(int &num1, int &num2) {
+    bool error;
+    cout<<"How many balls (1-12) are in the pool to pick from? ";
+    cin>>num1;
+    error = cin.fail();
 
+    while (num1>12) {
+        cout << "Input Error!  There must be between 1 and 12 balls."<< endl;
+        cout<<"How many balls (1-12) are in the pool to pick from? ";
+        cin>>num1;
+    }
+    while (error) {
+        cout<<"Input Error!"<<endl;
+        cout<<"How many balls (1-12) are in the pool to pick from? ";
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin>>num1;
+        error = cin.fail();
+    }
+    cout<<"How many balls (1-"<< num1 << ") will be drawn? ";
+    cin>>num2;
+    error = cin.fail();
+    while (num2>num1) {
+        cout << "Input Error!"<< endl;
+        cout<<"How many balls (1-"<< num1 << ") will be drawn? ";
+        cin>>num2;
+    }
+    while (error) {
+        cout<<"Input Error!"<<endl;
+        cout<<"How many balls (1-"<< num1 << ") will be drawn? ";
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cin>>num2;
+        error = cin.fail();
+    }
+
+}
+double computeWays(int num1, int num2) {
+double ways;
+    double fnum1=factorial(num1);
+    double fnum2=factorial(num2);
+    double f12num=factorial(num1-num2);
+    ways = (fnum2*f12num)/fnum1;
+    return ways;
+}
 /*******************************************************************
 * getLotteryInfo                                                   *
 * Gets and validates lottery info from the user and places it in   *
